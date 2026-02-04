eea960c8f36812f871bf11a99e32b8596ebfb12c
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-04 20:34:23 +0300

    add error and home route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styles from './components/Site.module.css';
 import {PageOne} from './components/pages/PageOne.tsx';
 import {PageTwo} from './components/pages/PageTwo.tsx';
 import {PageThree} from './components/pages/PageThree.tsx';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import {Error404} from './components/pages/Error404.tsx';
 
 
@@ -16,16 +16,20 @@ export const App = () => {
         </div>
         <div className={styles.content}>
           <Routes>
+            <Route path="/" element={<Navigate to={'/page1'}/>}/>
             <Route path="/page1" element={<PageOne/>}/>
-          <Route path="/page2"  element={<PageTwo/>} />
-        <Route path="/page3"  element={<PageThree/>} />
-            <Route path="*" element={<Error404 />}/>
-      </Routes>
+            <Route path="/page2" element={<PageTwo/>}/>
+            <Route path="/page3" element={<PageThree/>}/>
 
+
+            <Route path="/error404" element={<Error404/>}/>
+            <Route path="*" element={<Navigate to={'/error404'}/>}/>
+          </Routes>
+
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2026</div>
     </div>
-</div>
-  <div className={styles.footer}>abibas 2026</div>
-</div>
-)
-  ;
+  )
+    ;
 }
