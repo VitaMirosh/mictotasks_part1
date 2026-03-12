934d5450b1965f54d2378cb0769256bbaa22ae44
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-12 20:30:59 +0300

    add:router paths to component

---
+++ src/routes/router.tsx
@@ -0,0 +1,52 @@
+import {
+  createBrowserRouter,
+} from 'react-router-dom';
+import {App} from '../App.tsx';
+import {Error404} from '../components/pages/Error404.tsx';
+import {Adidas} from '../components/pages/Adidas.tsx';
+import {Puma} from '../components/pages/Puma.tsx';
+import {Abibas} from '../components/pages/Abibas.tsx';
+import {Model} from '../components/pages/Model.tsx';
+import {Prices} from '../components/pages/Prices.tsx';
+
+
+
+export const PATH = {
+  ADIDAS: '/adidas',
+  PUMA: '/puma',
+  ABIBAS: '/abibas',
+  PRICES: '/prices',
+  MODEL: '/:model/:id',
+} as const
+
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error404/>,
+    children: [
+      {
+        path: PATH.ADIDAS,
+        element: <Adidas/>,
+      },
+      {
+        path: PATH.PUMA,
+        element: <Puma/>,
+      },
+      {
+        path: PATH.ABIBAS,
+        element: <Abibas/>,
+      },
+      {
+        path: PATH.PRICES,
+        element: <Prices/>,
+      },
+      {
+        path: PATH.MODEL,
+        element: <Model/>,
+      },
+    ],
+  },
+]);
+
